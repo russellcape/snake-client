10a2f754d7e7f624d28e4b0e556ac7231875ad9e
Russell Cape <russellcape98@gmail.com>
2019-10-23 20:22:30 +0000

    Connect function moved into client file/ Exports connect also

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.0.103',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8'); 
+  
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+
+module.exports = connect;
