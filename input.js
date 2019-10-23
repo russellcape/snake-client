58e30e2cf778d4f95c5a953b7e14bcc096ef60bb
Russell Cape <russellcape98@gmail.com>
2019-10-23 21:58:33 +0000

    Added the input file and moved the 'setupInput' function along with the 'handleUserInput' function inside

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    console.log(key)
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+module.exports = setupInput;
