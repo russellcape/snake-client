b6fac28d090be00112cae8d35e197a712881a798
Russell Cape <russellcape98@gmail.com>
2019-10-23 20:53:17 +0000

    adding conn.write() to send messages to the server... (conn = connect function/ conn is a variable)

--- client.js
+++ client.js
@@ -12,7 +12,10 @@ const connect = function() {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+  conn.write('Name: RMC')
+  })
   return conn;
 }
 
