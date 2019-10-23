56d062b1a801e4573231fcdd287b2f97904427c7
Russell Cape <russellcape98@gmail.com>
2019-10-23 22:27:20 +0000

    Added the movement commands

--- input.js
+++ input.js
@@ -1,4 +1,9 @@
-const setupInput = function() {
+let connection;
+
+
+
+const setupInput = function(conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,10 +14,24 @@ const setupInput = function() {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === 'w') {
+      conn.write("Move: up")
+    }
+    if (key === 's') {
+      conn.write("Move: down")
+    }
+    if (key === 'd') {
+      conn.write("Move: right")
+    }
+    if (key === 'a') {
+      conn.write("Move: left")
+    }
   };
 
   stdin.on('data', handleUserInput);
   return stdin;
 };
+
+
 
 module.exports = setupInput;
